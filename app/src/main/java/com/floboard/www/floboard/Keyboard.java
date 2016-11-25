0b4b3697bea456e543b1537c76de2382481c4d46
OptimizedEfficiency <tyler.y.xu@hotmail.com>
2016-11-25 23:26:14 +0000

    Should have fixed AndroidManifest.xml

---
+++ app/src/main/java/com/floboard/www/floboard/Keyboard.java
@@ -0,0 +1,50 @@
+package com.floboard.www.floboard;
+
+import android.inputmethodservice.KeyboardView;
+import android.inputmethodservice.InputMethodService;
+
+/**
+ * Created by tyler on 11/25/2016.
+ */
+
+public class Keyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int i, int[] ints) {
+
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
